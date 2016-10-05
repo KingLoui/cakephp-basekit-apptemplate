92ef60347a703a54651e1519cf93a0ae1d9c3874
KingLoui <matzehollerbach@web.de>
2016-10-05 09:48:40 +0200

    cleanup

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -12,16 +12,5 @@ class AppController extends Controller
         parent::initialize();
         $this->loadComponent('KingLoui/BaseKit.BaseKit');
         $this->loadComponent('KingLoui/BaseKitUsers.BaseKitUsers');
-        // $this->loadComponent('Auth', [
-        //     'authorize' => ['Controller'],
-        //     'loginRedirect' => '/admin',
-        //     'logoutRedirect' => [
-        //         'prefix' => false,
-        //         'plugin' => false,
-        //         'controller' => 'Users',
-        //         'action' => 'login'
-        //     ],
-        //     'unauthorizedRedirect' => false
-        // ]);
     }
 }
